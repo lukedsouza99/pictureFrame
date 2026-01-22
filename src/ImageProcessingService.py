38afb97aa5d111c8f03d2a8e8d9d198bc5efbeba
Luke D'Souza <lukedsouza99@outlook.com>
2026-01-22 22:57:59 +0000

    added rotating to service

--- src/ImageProcessingService.py
+++ src/ImageProcessingService.py
@@ -12,5 +12,8 @@ class ImageProcessingService:
     def processImage(self, unprocessedImage):
         # resize / rotate depending on height and width
         image = Image.open(unprocessedImage)
+        width, height = image.size
+        if (height > width):
+            image = image.rotate(90)
         resizedImage = image.resize((800,480))
         return resizedImage
